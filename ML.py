d317cbcc0a3f255f23cbc552813c9ea2ffc3f724
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-03-21 10:34:31 +0300

    Update ML.py

--- ML.py
+++ ML.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from scikit-learn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 
 from sklearn.tree import DecisionTreeRegressor
